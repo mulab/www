640ce2bd2155ab97358cf43155fb7540b79ce0dd
Glassices <glassices@gmail.com>
2013-08-21 18:20:38 +0800

    Small Changes

--- animate/animate.js
+++ animate/animate.js
@@ -32,16 +32,44 @@ function DoDisplacement()
     cnt.style.webkitTransformOrigin = a+"px "+b+"px";
     cnt.style.webkitTransform = "scale(0.05, 0.05) translate(0px, 0px)";
     cnt.style.webkitTransition = "all 1s 0s linear";
+	cnt.style.mozTransformOrigin = a+"px "+b+"px";
+    cnt.style.mozTransform = "scale(0.05, 0.05) translate(0px, 0px)";
+    cnt.style.mozTransition = "all 1s 0s linear";
+	cnt.style.oTransformOrigin = a+"px "+b+"px";
+    cnt.style.oTransform = "scale(0.05, 0.05) translate(0px, 0px)";
+    cnt.style.oTransition = "all 1s 0s linear";
+	cnt.style.msTransformOrigin = a+"px "+b+"px";
+    cnt.style.msTransform = "scale(0.05, 0.05) translate(0px, 0px)";
+    cnt.style.msTransition = "all 1s 0s linear";
+	cnt.style.transformOrigin = a+"px "+b+"px";
+    cnt.style.transform = "scale(0.05, 0.05) translate(0px, 0px)";
+    cnt.style.transition = "all 1s 0s linear";
 }
 function DoAnimate()
 {
     var cnt1 = document.getElementById("myAnimate_Hi");
     cnt1.style.webkitTransform = "scale(1, 0) translate(0, 0)";
     cnt1.style.webkitTransition = "all 1s 0s ease-in";
+    cnt1.style.mozTransform = "scale(1, 0) translate(0, 0)";
+    cnt1.style.mozTransition = "all 1s 0s ease-in";
+    cnt1.style.oTransform = "scale(1, 0) translate(0, 0)";
+    cnt1.style.oTransition = "all 1s 0s ease-in";
+    cnt1.style.msTransform = "scale(1, 0) translate(0, 0)";
+    cnt1.style.msTransition = "all 1s 0s ease-in";
+    cnt1.style.transform = "scale(1, 0) translate(0, 0)";
+    cnt1.style.transition = "all 1s 0s ease-in";
     
     var cnt2 = document.getElementById("myAnimate_Mu");
     cnt2.style.webkitTransform = "scale(1, 1) translate(0, 0)";
     cnt2.style.webkitTransition = "all 1s 1s ease-out";
+	cnt2.style.mozTransform = "scale(1, 1) translate(0, 0)";
+    cnt2.style.mozTransition = "all 1s 1s ease-out";
+	cnt2.style.oTransform = "scale(1, 1) translate(0, 0)";
+    cnt2.style.oTransition = "all 1s 1s ease-out";
+	cnt2.style.msTransform = "scale(1, 1) translate(0, 0)";
+    cnt2.style.msTransition = "all 1s 1s ease-out";
+	cnt2.style.transform = "scale(1, 1) translate(0, 0)";
+    cnt2.style.transition = "all 1s 1s ease-out";
 }
 $(document).ready(function() {
     if ($(window).width() >= 768 && $(window).height() >= 400) {
@@ -64,6 +92,9 @@ $(document).ready(function() {
                 backgroundColor: '#000',
                 '-webkit-border-radius': '10px',
                 '-moz-border-radius': '10px',
+                '-o-border-radius': '10px',
+                '-ms-border-radius': '10px',
+                'border-radius': '10px',
                 opacity: .8,
                 color: '#fff'
             }
@@ -76,12 +107,7 @@ $(document).ready(function() {
         cnt2.style.left = ($(window).width()-$(cnt2).width())/2+'px';
         cnt2.style.top = (400-$(cnt2).height())/2+'px';
 
-        //alert($(document.getElementById("imageLabMuIcon")).height());
-        //alert(getElementLeft(document.getElementById("imageLabMuIcon")));
-        //alert(getElementTop(document.getElementById("imageLabMuIcon")));
         var t1 = setTimeout("DoAnimate()", 1000);
         var t3 = setTimeout("DoDisplacement()", 3000);
-        //alert($(cnt).height());
-        //setTimeout($.unblockUI, 2000);
     }
 });
