bbc8f8981413051968ffc6577ba8750a11bd2f21
Glassices <glassices@gmail.com>
2013-10-15 00:59:39 +0800

    ...

--- base.js
+++ base.js
@@ -5,12 +5,12 @@ function min(x, y) {
         return y;
 }
 window.onresize = function() {
-    h = min($(window).height(), $("#cutBackgroundImg").height());
+    //h = min($(window).height(), $("#cutBackgroundImg").height());
     //$("#cutBackgroundImg").css("clip", "rect(0px "+$(window).width()+"px "+$(window).height()+"px 0px)");
-    $("#myCarousel").css("height", h+"px");
+    //$("#myCarousel").css("height", h+"px");
 }
 window.onload = function() {
-    h = min($(window).height(), $("#cutBackgroundImg").height());
+    //h = min($(window).height(), $("#cutBackgroundImg").height());
     //$("#cutBackgroundImg").css("clip", "rect(0px "+$(window).width()+"px "+$(window).height()+"px 0px)");
-    $("#myCarousel").css("height", h+"px");
+    //$("#myCarousel").css("height", h+"px");
 }
