055e5e96ac3333b91d172d945222e0940c180d9d
hqythu <hqythu@gmail.com>
2014-10-06 02:53:09 +0800

    动画使用flipInY, flipOutY，解决动画之前的bug

--- app/scripts/animate.js
+++ app/scripts/animate.js
@@ -1,25 +1,32 @@
 'use strict';
 
-var state = 0;
+var state;
 var count = 2;
 var stateArray = ['simplify', 'design_code'];
 
 function performAminate() {
-    $('#'+stateArray[state]).removeClass().addClass('animated bounceOutLeft').
+    $('#'+stateArray[state]).removeClass().addClass('animated flipOutY').
         one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
             $(this).removeClass();
             $(this).hide(0, function() {
                 state = (state + 1) % count;
-                $('#'+stateArray[state]).show();
-                $('#'+stateArray[state]).removeClass().addClass('animated bounceInLeft').
-                    one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                        $(this).removeClass();
+                $('#'+stateArray[state]).show(0, function() {
+                    $('#'+stateArray[state]).removeClass().addClass('animated flipInY').
+                        one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                            $(this).removeClass();
                     });
+                });
             });
-        });
+    });
 }
 
 $(document).ready(function() {
-    $('#design_code').hide();
-    window.setInterval(performAminate, 5000);
+    state = 0;
+    $('#design_code').hide(0, function() {
+        $('#simplify').removeClass().addClass('animated flipInY').
+            one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass();
+        });
+        window.setInterval(performAminate, 5000);
+    });
 });
