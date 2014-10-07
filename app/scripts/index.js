4270d6614020baa73694985c48c7823714d4f8c3
hqythu <hqythu@gmail.com>
2014-10-08 03:04:10 +0800

    adjust position

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,4 +5,7 @@ console.log('\'Allo \'Allo!');
 $(document).ready(function () {
   var h = $('#simplify').height();
   $('.bubble').height(h);
+  $('#back').height($('#slogan').outerHeight());
+  $('#back-bottom').offset({top:($('body').height() - $('#footer').outerHeight())});
+  $('#back-bottom').height($('#footer').outerHeight());
 });
