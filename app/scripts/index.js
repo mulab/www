8c0716d995c7d4fedd675e45210727fe26d2ecdb
hqythu <hqythu@gmail.com>
2014-10-17 23:36:19 +0800

    fix error of slogan, in case js run a head of svg being fully loaded

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,8 +3,6 @@
 console.log('\'Allo \'Allo!');
 
 $(document).ready(function () {
-  var h = $('#simplify').height();
-  $('.bubble').height(h);
   $('#back').height($('#slogan').outerHeight());
   $('#back-bottom').offset({top:($('body').height() - $('#footer').outerHeight())});
   $('#back-bottom').height($('#footer').outerHeight());
