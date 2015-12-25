061f3a04491382751ae6d1081bac38ac1aad97b3
TennyZhuang <zty0826@gmail.com>
2015-12-25 19:00:40 +0800

    add: footer add: download icon circle fix: change header to main fix: screenshot fix: margin on different screen remove: nav comment

---
+++ app/scripts/tunet-desktop.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zhuangtianyi on 15/12/25.
+ */
+
+'use strict';
+
+$(function() {
+  var $main = $('#main');
+  $(window).on('load resize', function() {
+    var height = $(window).height() - $main.height() - $('footer').height();
+    $main.css('margin-top', ((height > 0)? height / 2.3: 0).toString() + 'px');
+  });
+});
