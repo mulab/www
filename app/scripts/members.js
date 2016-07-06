fe417ea30cee17bfb73dbf55ebc8d52ef32e6294
Ben Pig Chu <benpichu@gmail.com>
2016-07-07 00:06:39 +0800

    member animation part1

--- app/scripts/members.js
+++ app/scripts/members.js
@@ -1,153 +1,91 @@
 'use strict';
 
 $(document).ready(function () {
-  var $head = $('<div></div>').addClass('header');
-  $head.css({height: $(document).width() * 656 / 1300 + 'px'});
-  $('#nav').after();
-  var w;
-  if ($(document).width() > 768) {
-    w = 5;
-  }
-  else {
-    w = 3;
-  }
-  $.getJSON('members.html.json', function (data) {
-    var manager = $('#manager');
-    var i, j = 0;
-    var l = data.manager.length;
-    var member = data.manager;
-    var tr, img;
-    for (i = 0; i < l; i++) {
-      if (j === 0) {
-        tr = $('<div></div>').addClass('mytr');
+  var itemPerType = $('.type').map(function(){
+    return $(this).find('.headimg').length;
+  });
+  var itemPerLine = 0;
+  var current = null;
+  var currentElementIndex = -1;
+  var currentTypeIndex = -1;
+  var recenter = function(){
+    itemPerLine = parseInt($('.mytr').width() / $('.headimg').width());
+    var distance = $('.mytr').width() % $('.headimg').width();
+    $('.data-wrap').css('transform', 'translateX(' + distance / 2 + 'px)');
+  };
+  var resetTransform = function(){
+    $('.headimg').css('transform', '');
+    $('.type').css('transform', '');
+    if(currentElementIndex >= 0 && currentTypeIndex >= 0) {
+      var linePerType = itemPerType.map(function(_, e){
+        return 1 + (e - 1 - (e - 1) % itemPerLine) / itemPerLine;
+      });
+      var currentLineNumber = (currentElementIndex - currentElementIndex % itemPerLine) / itemPerLine;
+      var $currentType = $($('.type')[currentTypeIndex]);
+      var transformDistance = Math.max($('.tag-left').height(), $('.tag-right').height()) + 80;//padding
+      $currentType.nextAll().css('transform', 'translateY(' + transformDistance + 'px)');
+      $($currentType.find('.headimg')[itemPerLine * (1 + currentLineNumber) - 1]).nextAll().css('transform', 'translateY(' + transformDistance + 'px)');
+      var totalLineNumber = 0;
+      for(var i = currentTypeIndex; i < linePerType.length; i++){
+        totalLineNumber += linePerType[i];
       }
-      img = ($('<div></div>').addClass('flipper'));
-      img.append(($('<div></div>').addClass('front')).append($('<img>').attr({
-        'src': member[i].url,
-        'width': '100%'
-      })));
-      img.append((($('<div></div>').addClass('back')).append($('<h3></h3>').text(member[i].name))).append($('<h5></h5>').text(member[i].post)));
-      tr.append(($('<div></div>').addClass('headimg')).append((($('<div></div>').addClass('flip-container')).attr('ontouchstart', 'this.classList.toggle(\'hover\');').val(i)).append(img)));
-      j++;
-      if (j === w || i === (l - 1)) {
-        manager.append(tr);
-        j = 0;
-      }
+      totalLineNumber -= (currentLineNumber + 1);
+      console.log(totalLineNumber);
+      $('#tag').css('transform', 'translateY(-' + (totalLineNumber * $('.headimg').height()) + 'px)');
     }
-
-    manager = $('#consultant');
-    l = data.consultant.length;
-    j = 0;
-    member = data.consultant;
-    for (i = 0; i < l; i++) {
-      if (j === 0) {
-        tr = $('<div></div>').addClass('mytr');
-      }
-      img = ($('<div></div>').addClass('flipper'));
-      img.append(($('<div></div>').addClass('front')).append($('<img>').attr({
-        'src': member[i].url,
-        'width': '100%'
-      })));
-      img.append((($('<div></div>').addClass('back')).append($('<h3></h3>').text(member[i].name))).append($('<h5></h5>').text(member[i].post)));
-      tr.append(($('<div></div>').addClass('headimg')).append((($('<div></div>').addClass('flip-container')).attr('ontouchstart', 'this.classList.toggle(\'hover\');').val(i)).append(img)));
-      j++;
-      if (j === w || i === (l - 1)) {
-        manager.append(tr);
-        j = 0;
-      }
+  };
+  recenter();
+  $(window).on('resize', function(){
+    recenter();
+    resetTransform();
+  });
+  var removeOld = function(callback){
+    if(current !== null){
+      $('#tag').addClass('closed');
+      $('#tag').one('transitionend', callback);
+      $('.headimg').css('transition', 'transform 300ms');
+      $('.type').css('transition', 'transform 300ms');
+      $('.headimg').css('transform', '');
+      $('.type').css('transform', '');
+      $('.data-wrap').one('transitionend', function(){
+        $('.headimg').css('transition', '');
+        $('.type').css('transition', '');
+      });
+    } else {
+      callback();
     }
-
-    manager = $('#developer');
-    l = data.developer.length;
-    j = 0;
-    member = data.developer;
-    for (i = 0; i < l; i++) {
-      if (j === 0) {
-        tr = $('<div></div>').addClass('mytr');
-      }
-      img = ($('<div></div>').addClass('flipper'));
-      img.append(($('<div></div>').addClass('front')).append($('<img>').attr({
-        'src': member[i].url,
-        'width': '100%'
-      })));
-      img.append((($('<div></div>').addClass('back')).append($('<h3></h3>').text(member[i].name))).append($('<h5></h5>').text(member[i].post)));
-      tr.append(($('<div></div>').addClass('headimg')).append((($('<div></div>').addClass('flip-container')).attr('ontouchstart', 'this.classList.toggle(\'hover\');').val(i)).append(img)));
-      j++;
-      if (j === w || i === (l - 1)) {
-        manager.append(tr);
-        j = 0;
-      }
+  };
+  var loadNew = function(target){
+    if(target !== null){
+      current = $(target).data('people').name;
+      var $headimg = $(target);
+      var data = $headimg.data('people');
+      $('#tag-name').html(data.name);
+      $('#tag-img').prop('src', data.url);
+      $('#tag-link').prop('href', data.page);
+      $('#tag').removeClass('closed');
+      console.log($('#tag').height());
+      currentElementIndex = $headimg.prevAll().length;
+      currentTypeIndex = $headimg.parent().parent().prevAll().length;
+      $('.headimg').css('transition', 'transform 300ms');
+      $('.type').css('transition', 'transform 300ms');
+      resetTransform();
+      $('.data-wrap').one('transitionend', function(){
+        $('.headimg').css('transition', '');
+        $('.type').css('transition', '');
+      });
+    } else {
+      current = null;
+      currentElementIndex = -1;
+      currentTypeIndex = -1;
     }
-
-    manager = $('#others');
-    l = data.others.length;
-    member = data.others;
-    for (i = 0; i < l; i++) {
-      if (j === 0) {
-        tr = $('<div></div>').addClass('mytr');
-      }
-      img = ($('<div></div>').addClass('flipper'));
-      img.append(($('<div></div>').addClass('front')).append($('<img>').attr({
-        'src': member[i].url,
-        'width': '100%'
-      })));
-      img.append((($('<div></div>').addClass('back')).append($('<h3></h3>').text(member[i].name))).append($('<h5></h5>').text(member[i].post)));
-      tr.append(($('<div></div>').addClass('headimg')).append((($('<div></div>').addClass('flip-container')).attr('ontouchstart', 'this.classList.toggle(\'hover\');').val(i)).append(img)));
-      j++;
-      if (j === w || i === (l - 1)) {
-        manager.append(tr);
-        j = 0;
-      }
+  };
+  $('.headimg').on('click', function(event){
+    if( current === null || current !== $(event.currentTarget).data('people').name){
+      removeOld(loadNew.bind(this, event.currentTarget));
     }
   });
-  var display = false;
-  $('body').on('click', '.flip-container', function () {
-    var $tag = $('.tag');
-    if (!$(this).hasClass('hover')) {
-      if (display) {
-        $tag.slideToggle('fast');
-        $tag.remove();
-        $('.hover').removeClass('hover');
-      }
-      display = true;
-      $(this).addClass('hover');
-      var $em = $('<em></em>'), hw;
-      if (w === 5) {
-        hw = 20;
-      }
-      else {
-        hw = 7;
-      }
-      $em.css({
-        left: ($(this).parent()).position().left + ($(this).width() / 2) - hw + 'px'
-      });
-      var block = ($('<div></div>').addClass('tag')).append($em);
-      var card = ($('<div></div>').addClass('card'));
-      var info = ($('<div></div>').addClass('discription'));
-      var cat = ((($(this).parent()).parent()).parent()).attr('id');
-      var i = $(this).val();
-      $.getJSON('members.html.json', function (data) {
-        card.append(($('<div></div>').addClass('top')).append($('<h1></h1>').text(data[cat][i].name)));
-        card.append(($('<div></div>').addClass('img')).append($('<img>').attr({
-          'src': data[cat][i].url,
-          'width': '100%'
-        })));
-        card.append(($('<div></div>').addClass('tail')).append(($('<input>').addClass('button')).attr({
-          'type': 'bottom',
-          'onclick': 'location.href=' + data[cat][i].page,
-          'value': 'BLOG'
-        })));
-      });
-      block.append($('<left></left>').append(card));
-      block.append($('<right></right>').append(info));
-      block.append($('<div></div>').addClass('clear'));
-      (($(this).parent()).parent()).append(block.hide());
-      block.slideToggle('fast');
-    }
-    else {
-      $(this).removeClass('hover');
-      $tag.slideToggle('fast');
-      $tag.remove();
-    }
+  $('.fliper').on('transitionend', function(event){
+    event.stopPropagation();
   });
 });
